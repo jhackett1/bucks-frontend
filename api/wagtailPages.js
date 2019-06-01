e5bcce94d93b8a260b12e4838a35160e837f43ba
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2019-06-01 16:12:46 +0100

    add getPageByPath method

--- api/wagtailPages.js
+++ api/wagtailPages.js
@@ -1,21 +1,26 @@
 import fetch from 'isomorphic-unfetch'
 
+// TODO: Abstract into config
 const host = "http://localhost:8000"
 
 export const getLifeEvents = async () => {
     const res = await fetch(`${host}/api/v2/pages/?type=lifeevents.LifeEventPage`) 
-    const data = await res.json()
-    return data
+    return await res.json()
 }
 
 export const getPageById = async (id) => {
     const res2 = await fetch(`${host}/api/v2/pages/${id}`) 
-    const data = await res2.json()
-    return data
+    return await res2.json()
 }
 
 export const getPageBySlug = async (slug) => {
-    const res = await fetch(`http://localhost:8000/api/v2/pages?slug=${slug}`) 
+    // TODO: add proper error handling here
+    const res = await fetch(`${host}/api/v2/pages?slug=${slug}`) 
     const results = await res.json()
     return await getPageById(results.items[0].id)
+}
+
+export const getPageByPath = async (path) => {
+    const res = await fetch(`${host}/api/v2/pages/find/?html_path=${path}`) 
+    return await res.json()
 }
