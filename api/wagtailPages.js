5c65da97b56d1ce991010e7d22a4e6fdad4beefc
Jaye <joshua.hackett93@gmail.com>
2019-06-01 18:30:08 +0100

    move api host into config file and update vars

--- api/wagtailPages.js
+++ api/wagtailPages.js
@@ -1,26 +1,24 @@
 import fetch from 'isomorphic-unfetch'
-
-// TODO: Abstract into config
-const host = "http://localhost:8000"
+import {apiHost} from '../config.js'
 
 export const getLifeEvents = async () => {
-    const res = await fetch(`${host}/api/v2/pages/?type=lifeevents.LifeEventPage`) 
+    const res = await fetch(`${apiHost}/api/v2/pages/?type=lifeevents.LifeEventPage`) 
     return await res.json()
 }
 
 export const getPageById = async (id) => {
-    const res2 = await fetch(`${host}/api/v2/pages/${id}`) 
+    const res2 = await fetch(`${apiHost}/api/v2/pages/${id}`) 
     return await res2.json()
 }
 
 export const getPageBySlug = async (slug) => {
     // TODO: add proper error handling here
-    const res = await fetch(`${host}/api/v2/pages?slug=${slug}`) 
+    const res = await fetch(`${apiHost}/api/v2/pages?slug=${slug}`) 
     const results = await res.json()
     return await getPageById(results.items[0].id)
 }
 
 export const getPageByPath = async (path) => {
-    const res = await fetch(`${host}/api/v2/pages/find/?html_path=${path}`) 
+    const res = await fetch(`${apiHost}/api/v2/pages/find/?html_path=${path}`) 
     return await res.json()
 }
