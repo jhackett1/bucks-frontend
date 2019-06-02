d24c8d31deca75a0b8a38f1c0224b0dcfd9d87e0
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2019-06-03 00:04:28 +0100

    reset form when starting again

--- components/FinancialHelpChecker/Page4.jsx
+++ components/FinancialHelpChecker/Page4.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Page4 = ({changePage, kindOfHelp, anySavings}) => 
+const Page4 = ({reset, kindOfHelp, anySavings}) => 
     <>
         <h2 className="financial-checker__title">You may get some financial help</h2>
         
@@ -11,7 +11,7 @@ const Page4 = ({changePage, kindOfHelp, anySavings}) =>
         <p>Everyone has to pay something towards their residential care, but you may be entitled to financial help.</p>
         <p>We can help you find the best care options to suit you during a social care assessment.</p>
         <p>Your savings and income will be taken account during your assessment.</p>
-        <button className="financial-checker__button financial-checker__button--inverted" onClick={()=>{changePage(1)}}>Start again</button>
+        <button className="financial-checker__button financial-checker__button--inverted" onClick={()=>{reset()}}>Start again</button>
     </>
 
 export default Page4
--- components/FinancialHelpChecker/index.jsx
+++ components/FinancialHelpChecker/index.jsx
@@ -13,12 +13,18 @@ export default () => {
     const [kindOfHelp, answerKindOfHelp] = useState(null)
     const [anySavings, answerAnySavings] = useState(null)
 
+    const reset = () => {
+        changePage(1)
+        answerKindOfHelp(null)
+        answerAnySavings(null)
+    }
+    
     return(
         <aside className="financial-checker">
             {(page === 1) && <Page1 changePage={changePage}/> }
             {(page === 2) && <Page2 changePage={changePage} kindOfHelp={kindOfHelp} answerKindOfHelp={answerKindOfHelp} /> }
             {(page === 3) && <Page3 changePage={changePage} anySavings={anySavings} answerAnySavings={answerAnySavings}/> }
-            {(page === 4) && <Page4 changePage={changePage} kindOfHelp={kindOfHelp} anySavings={anySavings}/> }
+            {(page === 4) && <Page4 reset={reset} kindOfHelp={kindOfHelp} anySavings={anySavings}/> }
         </aside>
     )
 }
