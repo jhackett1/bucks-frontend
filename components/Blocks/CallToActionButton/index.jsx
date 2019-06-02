05c154d3db9069dde76f36c530a49da9c8923d1c
Jaye <joshua.hackett93@gmail.com>
2019-06-02 12:36:46 +0100

    fix typo

--- components/Blocks/CallToActionButton/index.jsx
+++ components/Blocks/CallToActionButton/index.jsx
@@ -2,5 +2,5 @@ import React from 'react'
 
 import './style.scss'
 
-export default ({<textarea name="" id="" cols="30" rows="10"></textarea>, url}) =>
-    <a className="call-to-action-button" href={url}>{<textarea name="" id="" cols="30" rows="10"></textarea>}</a>
+export default ({text, url}) =>
+    <a className="call-to-action-button" href={url}>{text}</a>
