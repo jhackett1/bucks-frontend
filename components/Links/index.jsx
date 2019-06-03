8f6f3655cd3ba8f5875f037695322128066b5b82
Jaye <joshua.hackett93@gmail.com>
2019-06-03 22:10:46 +0100

    little dialog box search

--- components/Links/index.jsx
+++ components/Links/index.jsx
@@ -6,7 +6,7 @@ import arrow from './arrow.svg'
 
 export const LinkTile = ({link, external}) =>
     <Link href={link.url || link.meta.html_url}>
-        <a className="link-tile"  target={external && "blank"}>
+        <a className={(external)? "link-tile link-tile--external" : "link-tile"}  target={external && "blank"}>
             {link.image && <img className="link-tile__image" src={link.image.meta.download_url} alt={link.image.title} loading="lazy"/>}
             <h3 className="link-tile__title">{link.title}</h3>
             <p className="link-tile__summary">{link.summary}</p>
@@ -18,8 +18,10 @@ export default ({links, external}) =>
     <section className="links">
         <h2 className={(external)? "links__title links__title--external section-title" : "links__title section-title"}>{(external)? "External links" : "Learn more"}</h2>
         {external && <p className="links__lede">Discover more about this issue from these trusted websites</p>}
-        {links && links.map((link, i)=>
-            <LinkTile link={link} key={i} external={external}/>            
-        )}
+        <div class="links__list">
+            {links && links.map((link, i)=>
+                <LinkTile link={link} key={i} external={external}/>            
+            )}
+        </div>
     </section>
 
